be6f0be683a7d5a86b397772d4c2ee3b4c1d358a
Dirk Englert <dirk.englert@web.de>
2017-01-18 15:26:05 +0100

    Added retrieving assets

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,34 @@
 $(document).ready(function() {
 
-    $('[data-collection]').each()
-    var collection = new Vue({
-    el: '[data-collections]',
-    data: {
-        open: false
-    },
-    methods: {
-      loadAssets: function (collectionId) {
-          var that = this;
+    $('[data-collection]').each(function() {
+        var collectionID = $(this).data('collection-id');
 
-          if(!that.open) {
-              $.ajax({
-                    method: "GET",
-                    url: "/assets/" + collectionId
-                })
-                .done(function(json) {
-                    console.log(json);
-                    that.open = true;
-                });
-          }
-          else {
-              that.open = false;
-          }
+        var collection = new Vue({
+            el: '[data-collection-id="'+collectionID+'"]',
+            data: {
+                open: false,
+                assets: null
+            },
+            methods: {
+                loadAssets: function() {
+                    var that = this;
 
-      }
-    }
-  })
+                    if (!that.open) {
+                        $.ajax({
+                                method: "GET",
+                                url: "/assets/" + collectionID
+                            })
+                            .done(function(json) {
+                                that.assets = json;
+                                that.open = true;
+                            });
+                    } else {
+                        that.open = false;
+                    }
+
+                }
+            }
+        });
+    });
 
 });
