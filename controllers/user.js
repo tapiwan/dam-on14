b81572baa6603b0b8eb7df4432110cc39b8deaae
Dirk Englert <dirk.englert@web.de>
2017-01-18 11:15:37 +0100

    Removed hackathon-starter references

--- controllers/user.js
+++ controllers/user.js
@@ -285,8 +285,8 @@ exports.postReset = (req, res, next) => {
 
       const mailOptions = {
         to: user.email,
-        from: 'hackathon@starter.com',
-        subject: 'Your Hackathon Starter password has been changed',
+        from: 'reset@diam.com',
+        subject: 'Your DiAM password has been changed',
         text: `Hello,\n\nThis is a confirmation that the password for your account ${user.email} has just been changed.\n`
       };
       transporter.sendMail(mailOptions, (err) => {
@@ -359,8 +359,8 @@ exports.postForgot = (req, res, next) => {
       });
       const mailOptions = {
         to: user.email,
-        from: 'hackathon@starter.com',
-        subject: 'Reset your password on Hackathon Starter',
+        from: 'reset@diam.com',
+        subject: 'Reset your password for DiAM',
         text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
           Please click on the following link, or paste this into your browser to complete the process:\n\n
           http://${req.headers.host}/reset/${token}\n\n
