27cdb49f09c22b22092562a9133e468aef0a02fb
dominikspeh <mail@domi-speh.de>
2017-01-27 22:36:39 +0100

    New dropzone settings

---
+++ public/js/config.js
@@ -0,0 +1,12 @@
+// FONT AWESOM FILE CLASSSES FOR UPLOAD
+var fileClasses = {
+    "default": "fa-file-o",
+    "png" : "fa-file-image-o",
+    "jpg" : "fa-file-image-o",
+    "jpeg" : "fa-file-image-o",
+    "pdf" : "fa-file-pdf-o",
+    "vnd.openxmlformats-officedocument.wordprocessingml.document" : "fa-file-word-o",
+    "mp3" : "fa-file-audio-o",
+    "mid" : "fa-file-audio-o",
+    "wav" : "fa-file-audio-o"
+};
--- public/js/dropzone.js
+++ public/js/dropzone.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 
+
+
+
+
     if(document.querySelector("#template")) {
+
         var previewNode = document.querySelector("#template");
         previewNode.id = "";
         var previewTemplate = previewNode.parentNode.innerHTML;
@@ -19,14 +24,49 @@ $(document).ready(function() {
             thumbnailHeight: 80,
             parallelUploads: 20,
             previewTemplate: previewTemplate,
-            autoQueue: false, // Make sure the files aren't queued until manually added
-            previewsContainer: "#previews", // Define the container to display the previews
-            clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+            autoQueue: false,
+            previewsContainer: "#previews",
+            clickable: ".fileinput-button"
         });
 
+        // ON FILE ADD
         myDropzone.on("addedfile", function (file) {
-            // Hookup the start button
-            $(".upload-area").css('background', 'none');
+            var type = file.type.split('/');
+            type = type;
+
+
+            if(type[0] != "image") {
+                if(fileClasses[type[1]] == undefined) {
+                    file.previewElement.querySelector("[data-dz-thumbnail]").parentNode.innerHTML= "<i class='fa fa-4x "+fileClasses.default+"'></i>";
+                    file.previewElement.querySelector("[data-dz-type]").className= "label label-primary fa "+fileClasses.default;
+
+                }
+                else {
+                    file.previewElement.querySelector("[data-dz-thumbnail]").parentNode.innerHTML= "<i class='fa fa-4x "+fileClasses[type[1]]+"'></i>";
+                    file.previewElement.querySelector("[data-dz-type]").className= "label label-primary fa "+fileClasses[type[1]];
+                }
+
+
+
+            }
+            else {
+                if(fileClasses[type[1]] == undefined) {
+                    file.previewElement.querySelector("[data-dz-thumbnail]").parentNode.innerHTML= "<i class='fa fa-4x "+fileClasses.default+"'></i>";
+                    file.previewElement.querySelector("[data-dz-type]").className= "label label-primary fa "+fileClasses.default;
+
+                }
+                else {
+                    file.previewElement.querySelector("[data-dz-type]").className= "label label-primary fa "+fileClasses[type[1]];
+
+                }
+
+            }
+
+
+
+
+
+            $(".upload-area").removeClass('uploadImage');
             $(".start, .cancel").fadeIn();
 
             file.previewElement.querySelector(".start").onclick = function () {
@@ -34,28 +74,42 @@ $(document).ready(function() {
             };
         });
 
+        // ON FILE REMOVE
+        myDropzone.on("removedfile", function () {
+           if(myDropzone.files.length == 0){
+               $(".start, .cancel").fadeOut();
+               $(".upload-area").addClass('uploadImage');
+           }
 
+        });
+
+        // DISBALE BUTTONS AFTER UPLOAD
         myDropzone.on("sending", function (file) {
             file.previewElement.querySelector(".start").setAttribute("disabled", "disabled");
         });
 
-// Hide the total progress bar when nothing's uploading anymore
+        // UPLOAD SUCESS
         myDropzone.on("queuecomplete", function (progress) {
-            $(".start, .cancel").fadeOut();
+
 
         });
 
-// Setup the buttons for all transfers
-// The "add files" button doesn't need to be setup because the config
-// `clickable` has already been specified.
+        // UPLOAD ALL
         document.querySelector("#actions .start").onclick = function () {
             myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
         };
+
+        // CANCEL ALL
         document.querySelector("#actions .cancel").onclick = function () {
             myDropzone.removeAllFiles(true);
+            $(".upload-area").addClass('uploadImage');
             $(".start, .cancel").fadeOut();
+            console.log("remove")
+
+
 
         };
     }
+
 
 });
