1b05d774f0bc79d2aae5e36cfa6464bf8af948ef
Dirk Englert <dirk.englert@web.de>
2017-02-06 12:01:54 +0100

    Added "no collections"/"no activities" info

--- models/Activity.js
+++ models/Activity.js
@@ -12,25 +12,6 @@ const activitySchema = new mongoose.Schema({
 const Activity = mongoose.model('Activity', activitySchema);
 
 Activity.find(function (err, activities) {
-    if (activities.length) {
-        console.log("Activities Demo bereits vorhanden");
-        return;
-    }
-    new Activity({
-        user: "Dirk",
-        action: "updated XYZ.png"
-    }).save();
-
-    new Activity({
-        user: "Dirk",
-        action: "updated XYZ.png"
-    }).save();
-
-    new Activity({
-        user: "Dirk",
-        action: "updated XYZ.png"
-    }).save();
-
     console.log("Activities will be added when changes occur.");
 });
 module.exports = Activity;
